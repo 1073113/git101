37162eb067b42fbdc313165b1548dd75d77796f4
David Ho <davivho@hotmail.com>
2021-02-14 16:39:11 +0800

    A1 After Merge, then add new Output A1 yes

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'This is A1 from Main!';
+    return 'After Merge, then add new Output A1 yes!';
   }
   getHelloWorld(): string {
     return 'This is A1 getHelloWorld!';
